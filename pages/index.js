148fe7a7ed9c0e5e7a82cb5a21d43304f1b614f2
FedericoGVillalba <fvillalba@hexagonconsulting.net>
2022-10-19 20:27:03 -0300

    Config ESLint

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
 import AppLayout from '../components/AppLayout'
 import { colors } from '../styles/theme'
 import Button from '../components/Button'
