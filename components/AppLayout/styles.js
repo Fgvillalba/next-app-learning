b061222c8a65573b2b617e2479772f496b300f64
FedericoGVillalba <fvillalba@hexagonconsulting.net>
2022-10-30 11:23:52 -0300

    Style Home and Neuits Constants for User States

--- components/AppLayout/styles.js
+++ components/AppLayout/styles.js
@@ -7,12 +7,13 @@ const backgroundColor = addOpacityToColor(colors.primary, 0.3)
 export const globalStyles = css.global`
   html,
   body {
-    background-image: radial-gradient(${backgroundColor} 1px, #fdfdfd 1px),
-      radial-gradient(${backgroundColor} 1px, #fdfdfd 1px);
+    background-image: radial-gradient(${backgroundColor} 1px, #f8fffd 1px),
+      radial-gradient(${backgroundColor} 1px, #f2fffb 1px);
     background-position: 0 0, 25px 25px;
     background-size: 50px 50px;
     padding: 0;
     margin: 0;
+    overflow: hidden;
     font-family: ${fonts.base};
   }
 
@@ -31,10 +32,11 @@ export default css`
   main {
     position: relative;
     background: #fff;
-    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 10px 25px rgba(187, 180, 193, 0.8);
     border-radius: 10px;
     width: 100%;
     height: 100%;
+    overflow-y: auto;
   }
 
   @media (min-width: ${breakpoints.mobile}) {
