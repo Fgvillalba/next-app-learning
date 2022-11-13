36139fd78f9ba12b5e311f643ad13cf4d0d38540
Fgvillalba <f.gabrielvillalba@gmail.com>
2022-11-13 19:53:26 -0300

    add lint and prettier

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,36 +1,33 @@
-import { colors } from '../../styles/theme'
+import { colors } from "../../styles/theme";
 
 const Button = ({ children, onClick }) => {
   return (
     <>
-      <button onClick={onClick}>
-        {children}
-      </button>
+      <button onClick={onClick}>{children}</button>
       <style jsx>{`
         button {
-            background: ${colors.black};
-            color: ${colors.white};
-            border: 0;
-            border-radius: 9999px;
-            display: flex;
-            align-items: center;
-            font-weight: 800;
-            padding: 4px 24px;
-            cursor: pointer;
-            transition: opacity .3s ease;
-          }
+          background: ${colors.black};
+          color: ${colors.white};
+          border: 0;
+          border-radius: 9999px;
+          display: flex;
+          align-items: center;
+          font-weight: 800;
+          padding: 4px 24px;
+          cursor: pointer;
+          transition: opacity 0.3s ease;
+        }
 
         button > :global(svg) {
           margin-right: 8px;
         }
 
         button:hover {
-          opacity: .7;
+          opacity: 0.7;
         }
-        }  
       `}</style>
     </>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
