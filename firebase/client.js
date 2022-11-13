b5088dd3d74d10043db03f88c42911011719779e
Fgvillalba <f.gabrielvillalba@gmail.com>
2022-11-13 19:53:23 -0300

    Add helpers for auth state changed

--- firebase/client.js
+++ firebase/client.js
@@ -14,17 +14,27 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
-export const loginWithGitHub = () => {
-  const githubProvider = new GithubAuthProvider()
-  const auth = getAuth(app)
-  return signInWithPopup(auth, githubProvider)
-    .then(user => {
-      const {_tokenResponse} = user
+const mapUserFromFirebaseAuthToUser = (userFromFirebase) => {
+  const {_tokenResponse} = userFromFirebase
       const {screenName, photoUrl, email} = _tokenResponse
       return {
         avatar: photoUrl,
         userName: screenName,
         email
       }
+}
+
+
+export const onUserStateChanged = (onChange) => {
+  return onAuthStateChanged(user => {
+    const normalizedUser = mapUserFromFirebaseAuthToUser(user)
+    onChange(normalizedUser)
   })
+}
+
+export const loginWithGitHub = () => {
+  const githubProvider = new GithubAuthProvider()
+  const auth = getAuth(app)
+  return signInWithPopup(auth, githubProvider)
+    .then(mapUserFromFirebaseAuthToUser) //recibe el user y se lo pasa a la funcino como parametro
 }
